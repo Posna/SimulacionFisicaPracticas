049567f7c8536b547aacebc0c92bfc23ae0af3a6
Posna <jomart17@ucm.es>
2019-10-11 10:42:56 +0200

    Entrega 2 nuevo firework e inicio Entrega 3

---
+++ Entrega3/Practica1/skeleton/Particle.cpp
@@ -0,0 +1,71 @@
+#include "Particle.h"
+
+Particle::Particle(float radio, Vector4 c, Vector3 p): radio_(radio), color_(c), position_(p)
+{
+	t = new PxTransform(p);
+	particle_ = new RenderItem(CreateShape(PxSphereGeometry(radio)), t, c);
+}
+
+Particle::~Particle()
+{
+	delete t;
+	particle_->release();
+}
+
+void Particle::setVel(Vector3 v, float s)
+{
+	velocity_ = v;
+	speed_ = s;
+}
+
+void Particle::setAcceleration(Vector3 a)
+{
+	acceleration_ = a;
+}
+
+void Particle::setDumping(float d)
+{
+	damping_ = d;
+}
+
+void Particle::setMass(float mass)
+{
+	inverse_mass_ = 1.0 / mass;
+}
+
+void Particle::integrate(float time)
+{
+	// Trivial case, infinite mass --> do nothing
+	if (inverse_mass_ <= 0.0f) return;
+	// Update position
+	position_ += velocity_*speed_* time;
+
+	Vector3 totalAcc = Vector3(0);
+	totalAcc += force_ * inverse_mass_;
+	// Update linear velocity
+	velocity_ += acceleration_* time;
+	//Update linear velocity
+	velocity_ += totalAcc * time;
+	// Impose drag (damping)
+	velocity_ *= powf(damping_, time);
+	//Change the particles position
+	*t = PxTransform(position_);
+	clearForce();
+}
+
+void Particle::addForce(const Vector3& f)
+{
+	force_ += f;
+}
+
+bool Particle::update(float t)
+{
+	integrate(t);
+	age -= t;
+	return (age < 0);
+}
+
+void Particle::clearForce()
+{
+	force_ = Vector3(0);
+}
---
+++ Entrega3/Practica1/skeleton/Particle.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "RenderUtils.hpp"
+#include <PxPhysicsAPI.h>
+
+using namespace physx;
+
+class Particle
+{
+public:
+	Particle(float radio, Vector4 c = Vector4(0.5, 0.5, 0.5, 1), Vector3 p = Vector3(0.0f, 0.0f, 0.0f));
+	~Particle();
+
+	void setVel(Vector3 v, float s);
+	void setAcceleration(Vector3 a);
+	void setDumping(float d);
+	void setMass(float mass);
+	void integrate(float time);
+	void addForce(const Vector3& f);
+
+	bool update(float t);
+private:
+	void clearForce();
+
+	RenderItem* particle_;
+	PxTransform* t;
+
+	Vector4 color_;
+	Vector3 position_;
+	Vector3 velocity_ = Vector3(0,0,0);
+	Vector3 acceleration_ = Vector3(0, 0, 0);
+	Vector3 force_;
+
+	float speed_;
+	float radio_;
+	float damping_ = 0.999f;
+	float inverse_mass_ = 1.0f/30.0f;
+	float age = 1;
+};
